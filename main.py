8b0159b2a27160f471e356f1dcabd8e6b915d356
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-26 12:24:57 -0700

    Add graphs

--- main.py
+++ main.py
@@ -10,13 +10,23 @@ days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the num
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    filtered_data = get_data(place, days)
 
-filtered_data = get_data(place, days)
+    if option == "Temperature":
+        temperatures = [item['main']['temp'] for item in filtered_data]
+        dates = [item['dt_txt'] for item in filtered_data]
+        figure = ps.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
+        st.plotly_chart(figure)
 
-if option == "Temperature":
-    temperatures = [item['main']['temp'] for item in filtered_data]
-elif option == "Sky":
-    weather_conditions = [item['weather'][0]['main'] for item in filtered_data]
-
-# figure = ps.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
-# st.plotly_chart(figure)
+    elif option == "Sky":
+        weather_conditions = [item['weather'][0]['main'] for item in filtered_data]
+        dates = [item['dt_txt'] for item in filtered_data]
+        images = {
+            "Clear": "images/clear.png",
+            "Clouds": "images/cloud.png",
+            "Rain": "images/rain.png",
+            "Snow": "images/snow.png"
+        }
+        image_paths = [images[condition] for condition in weather_conditions]
+        st.image(image_paths, width=115)
