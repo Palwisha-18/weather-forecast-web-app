d423f04532d4c2c2e0e1ac41d54b90f3e1d32274
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-26 13:21:15 -0700

    Inactived old API_KEY. Add functionality to read new API_KEY from env file

--- backend.py
+++ backend.py
@@ -1,12 +1,19 @@
-import requests
+import os
+from os.path import join, dirname
 
-API_KEY = "3c0836b806ff47e77c284d14eb100c3d"
+import requests
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 def get_data(place, forecast_days):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={SECRET_KEY}"
     response = requests.get(url)
     data = response.json()
-    filtered_data = data['list'][:8*forecast_days]
+    filtered_data = data['list'][:8 * forecast_days]
 
     return filtered_data
